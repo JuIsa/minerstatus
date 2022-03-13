21d0040bc621db8d3df2c7733bbb96e33afa60f4
Isa <isami.dono@gmail.com>
2022-03-13 19:34:59 +0600

    -small update

--- miner.py
+++ miner.py
@@ -2,17 +2,23 @@ from requests.auth import HTTPDigestAuth
 import requests
 import sys
 
+
 addr = 'http://192.168.0.'
+
 end = '/cgi-bin/minerStatus.cgi'
 
 for i in range(2,255):
+
 	url = addr+str(i)+end
+
 	try:
-		req = requests.get(url, auth=HTTPDigestAuth('root', 'root'), timeout=0.1)
+		req = requests.get(url, auth=HTTPDigestAuth(user, password), timeout=0.1)
+
 	except:
-		print(f'fail {url}', flush=True)
+		print(f'fail {url}', flush=True)# flush for visual
+
 	else:
-		print(f'{req} {url}')
+		print(f'{req} {url}')# no flushing
 		break
 
 
